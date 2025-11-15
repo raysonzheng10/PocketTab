4f52c5bfb06981cf121799f8267d80fcb58aa998
Rayson Zheng <raysonzheng10@gmail.com>
2025-11-15 15:53:18 -0500

    factored layout content for home

--- src/app/demo/layout.tsx
+++ src/app/demo/layout.tsx
@@ -2,10 +2,10 @@
 import { ErrorProvider } from "../home/context/ErrorContext";
 import { UserProvider } from "../home/context/UserContext";
 import { GroupProvider } from "../home/context/GroupContext";
-import { HomeLayoutContent } from "../home/layout";
 import { SettlementProvider } from "../home/group/[groupId]/context/SettlementContext";
 import { TransactionProvider } from "../home/group/[groupId]/context/TransactionContext";
 import { RecurringTransactionProvider } from "../home/group/[groupId]/context/RecurringTransactionContext";
+import HomeLayoutContent from "../home/components/HomeLayoutContent";
 
 export default function DemoLayout({
   children,
